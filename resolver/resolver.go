251f80758e5efe2faa06c884559664d9e2960983
Mahdi <mhd.moalllem@gmail.com>
2025-12-24 19:59:49 +0330

    complete primary resolver

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -13,3 +13,29 @@ func NewResolver(s types.Storage, u types.UpStream) *DefaultResolver {
 		upstream: u,
 	}
 }
+
+func (r *DefaultResolver) Resolve(q types.DNSQuestion) types.DNSResponse {
+
+	// 1. check local storage / cache
+	if records, ok := r.storage.Get(q); ok {
+		return types.DNSResponse{
+			Records: records,
+			RCode:   0,
+		}
+	}
+
+	// 2. forward to upstream
+	resp, err := r.upstream.Query(q)
+	if err != nil {
+		return types.DNSResponse{
+			RCode: 2, // SERVFAIL
+		}
+	}
+
+	// 3. cache upstream response
+	for _, rec := range resp.Records {
+		r.storage.Set(rec)
+	}
+
+	return resp
+}
